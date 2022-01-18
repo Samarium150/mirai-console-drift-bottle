e60f7eb5416a8099a5a76c536bd8bea4c8b127d2
LaoLittle <y1941803875@outlook.com>
2022-01-18 21:25:16 +0800

    添加一个操作指令（或许以后能添加更多操作指令），目前只有删除

---
+++ src/main/kotlin/io/github/samarium150/mirai/plugin/driftbottle/command/DeleteBottle.kt
@@ -0,0 +1,27 @@
+package io.github.samarium150.mirai.plugin.driftbottle.command
+
+import io.github.samarium150.mirai.plugin.driftbottle.MiraiConsoleDriftBottle
+import io.github.samarium150.mirai.plugin.driftbottle.data.Sea
+import io.github.samarium150.mirai.plugin.driftbottle.util.indexOfBottle
+import net.mamoe.mirai.console.command.CommandSender
+import net.mamoe.mirai.console.command.CompositeCommand
+import net.mamoe.mirai.utils.error
+
+object DeleteBottle : CompositeCommand(
+    MiraiConsoleDriftBottle, "bottle", "b",
+    description = "漂流瓶操作指令"
+) {
+    @SubCommand("del", "rm")
+    suspend fun CommandSender.remove(index: Int? = subject?.let { indexOfBottle[it.id] }) {
+        if (index == null) {
+            subject?.sendMessage("删除失败，请尝试输入序号") ?: MiraiConsoleDriftBottle.logger.error { "控制台删除请输入序号" }
+            return
+        }
+        if (index < 0 || index > Sea.contents.size) {
+            sendMessage("数字超出范围！")
+            return
+        }
+        Sea.contents.removeAt(index)
+        sendMessage("已删除漂流瓶$index")
+    }
+}
