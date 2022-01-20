77128cd76684ec15c5eae97f7b3479907488767c
LaoLittle <y1941803875@outlook.com>
2022-01-20 13:39:43 +0800

    修复无法使用runTerminal调试

--- src/test/kotlin/io/github/samarium150/mirai/plugin/driftbottle/RunTerminal.kt
+++ src/test/kotlin/io/github/samarium150/mirai/plugin/driftbottle/RunTerminal.kt
@@ -1,36 +1,32 @@
-/**
- * Copyright (c) 2020-2021 Samarium
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>
- */
 package io.github.samarium150.mirai.plugin.driftbottle
 
+import net.mamoe.mirai.alsoLogin
 import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.enable
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.load
 import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
+import java.io.File
 
-@ConsoleExperimentalApi
+fun setupWorkingDir() {
+    // see: net.mamoe.mirai.console.terminal.MiraiConsoleImplementationTerminal
+    System.setProperty("user.dir", File("debug-sandbox").absolutePath)
+}
+
+@OptIn(ConsoleExperimentalApi::class)
 suspend fun main() {
+    setupWorkingDir()
 
     MiraiConsoleTerminalLoader.startAsDaemon()
 
     val pluginInstance = MiraiConsoleDriftBottle
 
-    pluginInstance.load()
-    pluginInstance.enable()
+    pluginInstance.load() // 主动加载插件, Console 会调用 GroupConn.onLoad
+    pluginInstance.enable() // 主动启用插件, Console 会调用 GroupConn.onEnable
+
+    val bot = MiraiConsole.addBot(123456, "password") {
+        fileBasedDeviceInfo("D:\\mirai\\bots\\779119352\\device.json")
+    }.alsoLogin() // 登录一个测试环境的 Bot
 
     MiraiConsole.job.join()
 }
